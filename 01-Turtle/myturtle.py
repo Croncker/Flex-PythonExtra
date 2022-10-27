e392cafe3492dcc5b77e50b3617bed2865784ad4
Lucca <35749@ma-web.nl>
2022-10-27 12:18:09 +0200

    Opdract FilesFolder

--- 01-Turtle/myturtle.py
+++ 01-Turtle/myturtle.py
@@ -1,18 +1,6 @@
 import turtle
 import time
 
-for x in range(4):
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    turtle.forward(100)
-    turtle.right(90)
-    
-turtle.done()
-
 venster = turtle.getscreen()
 venster.bgcolor("black")
 
@@ -30,4 +18,23 @@ turtle.begin_fill()
 
 turtle.circle(100)
 turtle.end_fill()
-time.sleep(10)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+
+turtle.fillcolor("#0AEFDE")
+turtle.begin_fill()
+
+turtle.circle(100)
+turtle.end_fill()
+
+turtle.pencolor("#0AEFDE")
+turtle.fillcolor("#FF0000")
+turtle.begin_fill()
+turtle.right(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+
+time.sleep(5)
+
